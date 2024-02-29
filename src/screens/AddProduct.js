7cd76cfe8346289bcba9ab4a20f9588c631be33f
Krishnansh Tiwari <krihnansht2003@gmail.com>
2024-02-29 15:03:52 +0530

    29

--- src/screens/AddProduct.js
+++ src/screens/AddProduct.js
@@ -1,9 +1,124 @@
-import React from 'react'
+import React from "react";
 
 function AddProduct() {
   return (
-    <div>AddProduct</div>
-  )
+    <>
+      <div className="min-h-[60vh] w-4/5 sm:w-2/5 mx-auto py-7">
+        <h1 className="text-4xl font-bold tracking-tight text-[#2d163f] mb-4">
+          Add Product
+        </h1>
+        <form>
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="title"
+                className="block text-sm font-medium leading-6 text-[#2d163f]"
+              >
+                Title
+              </label>
+            </div>
+            <div className="mt-2">
+              <input
+                id="title"
+                name="title"
+                type="title"
+                autoComplete="title"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-[#2d163f] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-800 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="price"
+                className="block text-sm font-medium leading-6 text-[#2d163f]"
+              >
+                Price
+              </label>
+            </div>
+            <div className="mt-2">
+              <input
+                id="price"
+                name="price"
+                type="number"
+                autoComplete="price"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-[#2d163f] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-800 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div class="mb-4">
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="price"
+                className="block text-sm font-medium leading-6 text-[#2d163f]"
+              >
+                Image
+              </label>
+            </div>
+            <div className="mt-2">
+              <input
+                type="file"
+                id="image"
+                name="image"
+                accept="image/*"
+                class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-purple-500 focus:purple-indigo-500 sm:text-sm "
+              />
+            </div>
+          </div>
+          <div class="mb-4">
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="description"
+                className="block text-sm font-medium leading-6 text-[#2d163f]"
+              >
+                Description
+              </label>
+            </div>
+            <div className="mt-2">
+              <textarea
+                id="description"
+                name="description"
+                rows="3"
+                className="block w-full rounded-md border-0 py-1.5 text-[#2d163f] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-800 sm:text-sm sm:leading-6"
+              ></textarea>
+            </div>
+          </div>
+          <div class="mb-4">
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="category"
+                className="block text-sm font-medium leading-6 text-[#2d163f]"
+              >
+                Category
+              </label>
+            </div>
+            <div className="mt-2">
+              <select
+                id="category"
+                name="category"
+                className="block w-full rounded-md border-0 py-1.5 text-[#2d163f] shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-inset placeholder:text-gray-400  focus:ring-purple-800 sm:text-sm sm:leading-6"
+              >
+                <option value="clothing">Clothing</option>
+                <option value="electronics">Electronics</option>
+                <option value="books">Books</option>
+                <option value="other">Other</option>
+              </select>
+            </div>
+          </div>
+          <div class="flex justify-end">
+          <button
+            type="submit"
+            className="flex text-xl justify-center rounded-md bg-[#4d2f62] px-4 py-2 font-bold leading-6 text-white shadow-sm hover:bg-purple-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Submit
+          </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default AddProduct
+export default AddProduct;
--- src/screens/Productdetail.js
+++ src/screens/Productdetail.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
 
 function Productdetail() {
   return (
-    <div>Productdetail</div>
-  )
+    <>
+      <div className="relative flex flex-col sm:flex-row w-[90%] mx-auto my-5 px-4 min-h-[60vh]">
+        <div className="sm:h-[40%] sm:w-[40%] w-full h-full flex-shrink-0 overflow-hidden rounded-md border border-gray-200 flex flex-col">
+          <h1 className="text-4xl text-center mt-5 font-bold tracking-tight text-[#2d163f] mb-4">
+            Leather Bag
+          </h1>
+          <img
+            src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg"
+            alt="img"
+            className="h-full w-full object-cover object-center"
+          />
+        </div>
+        <div className="flex flex-col p-5">
+          <p className="text-justify font-semibold text-[#5d476e]">
+            In this example, the flex class is applied to the container to make
+            it a flex container. The justify-between class is used to distribute
+            the items along the main axis with space between them. Each item has
+            a bg-gray-300 class for a gray background and a p-4 class for
+            padding. Adjust the classes as needed for your layout.
+          </p>
+          <div className="flex flex-row mt-4 font-bold text-xl text-[#2d163f] justify-between">
+            <div>$240</div>
+            <div>Bags</div>
+          </div>
+          <div className="mt-4 font-semibold text-lg text-[#2d163f]">
+            <h3>Seller : Shukla Abhishek</h3>
+          </div>
+          <div className="absolute bottom-0 right-0">
+          <button
+            type="submit"
+            className="flex text-xl justify-center rounded-md bg-[#4d2f62] px-4 py-2 font-bold leading-6 text-white shadow-sm hover:bg-purple-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Checkout
+          </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Productdetail
+export default Productdetail;
